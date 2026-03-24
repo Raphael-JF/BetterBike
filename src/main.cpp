415e13dc6403c98bcf7d088cdbb3c3ea39cca92f
Raph laptop <raphaeljontef@hotmail.com>
2026-03-24 22:44:47 +0100

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,109 +1,23 @@
-#include <math.h>
-#include <stdint.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
-#include <Wire.h>
-#include "Arduino.h"
-
-#include "main.h"
-#include "gps/gps_core.h"
-#include "time/time_manager.h"
-#include "lcd/lcd_core.h"
-
-
-
-
-
-
-
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(4, 3); // pins choisies pour UART shield
 
 void setup() {
-    // Serial.begin(115200); // esp
-    Serial.begin(9600); // arduino
-    // Wire.begin();
-    // set up the lcd's number of columns and rows:
-    lcd.begin(16, 2);
-    lcd.setRGB(127, 127, 127);
-
-
-    lcd.setCursor(0, 0);
-    lcd.print("--:-- GPS");
-
-    uint8_t gps_valid_char[8] = {
-        0b00000,
-        0b00001,
-        0b00010,
-        0b10100,
-        0b01000,
-        0b00000,
-        0b00000,
-        0b00000
-    };
-
-    uint8_t gps_invalid_char[8] = {
-        0b00000,
-        0b10001,
-        0b01010,
-        0b00100,
-        0b01010,
-        0b10001,
-        0b00000,
-        0b00000
-    };
-
-    lcd.createChar(6, gps_valid_char);
-    lcd.createChar(7, gps_invalid_char);
-    lcd_respring_gps_status();
-
-    lcd.setCursor(12, 0);
-    lcd.print("|");
-    lcd.setCursor(12, 1);
-    lcd.print("|");
-
-    lcd.setCursor(13, 0);
-
-    lcd.write(byte(0));
-    lcd.write(byte(1));
-    lcd.write(byte(2));
-    lcd.setCursor(13, 1);
-    lcd.write(byte(3));
-    lcd.write(byte(4));
-    lcd.write(byte(5));
-
-    lcd_respring_compass();
-
-
+    Serial.begin(9600);     // Pour le moniteur série
+    gpsSerial.begin(9600);    // UART GPS
 }
 
-
 void loop() {
-
-    bearing_to_display += 0.5;
-    if(bearing_to_display > 2*M_PI) {
-        bearing_to_display -= 2*M_PI;
+    while (gpsSerial.available()) {
+        gps.encode(gpsSerial.read());
     }
 
-
-    if (fabs(bearing_to_display - last_bearing_to_display) > 0.05) {
-        if (is_gps_active) {
-            clear_compass_for_gps();
-        }
-        else{
-            clear_compass_for_magnetometer();
-        }
-        lcd_respring_compass();
-        last_bearing_to_display = bearing_to_display;
+    if (gps.location.isUpdated()) {
+        Serial.print("Lat: ");
+        Serial.println(gps.location.lat(), 6);
+        Serial.print("Lng: ");
+        Serial.println(gps.location.lng(), 6);
     }
-
-    
-    struct time_base old_time = current_time;
-    update_time();
-    if (current_time.minutes != old_time.minutes || current_time.hours != old_time.hours){
-        lcd_respring_time();
-    }
-
-    // Serial.println("Starting LCD simulation...");
-
-    delay(500);
-    is_gps_active = 1;
-    last_got_gps_time = {12, 34};
 }
